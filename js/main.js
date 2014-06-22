fdaba0f5e2b0370280477389c6e6f622d69c8e21
Jordy Pouw <jordypouw@gmail.com>
2014-06-22 13:40:56 +0200

    Docs website launch V1.0

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('.scrollDown').on('click', function () {
+		$('#quickExample').scrollToBySpeed({
+			speed: 1000
+		});
+	});
+
+  $('.scrollDown2').on('click', function () {
+		$('#quickExample').scrollToBySpeed({
+			speed: 1000
+		});
+	});
+
+  $('.scrollDown3').on('click', function () {
+		$('#methods').scrollToBySpeed({
+			speed: 1000
+		});
+	});
+
+  $('.scrollDown4').on('click', function () {
+		$('#gettingStarted').scrollToBySpeed({
+			speed: 1000
+		});
+	});			
+
+});
