07e3005d2c3514368922487408d56c979e53b410
Jordy Pouw <jordypouw@gmail.com>
2014-06-24 20:19:58 +0200

    docs website

---
+++ js/plugins.js
@@ -0,0 +1,25 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place jQuery/helper plugins in here.
+// One approach is to put jQuery plugins inside of a `(function($){ ...})(jQuery);`
